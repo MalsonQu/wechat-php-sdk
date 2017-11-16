ff31f70f7070d47e39e7985807540ec666f137d4
Malson <quqingyu@live.cn>
2017-11-16 13:56:44 +0800

    加强验证错误提示

--- WeChat/Service.php
+++ WeChat/Service.php
@@ -9,6 +9,7 @@
 namespace WeChat;
 
 
+use WeChat\Exception\ConfigException;
 use WeChat\Tools\Verify;
 
 class Service extends Base
@@ -32,11 +33,22 @@ class Service extends Base
 
     /**
      * 验证微信服务器配置
-     *
      * @return bool
+     * @throws ConfigException
      */
     static public function verify ()
     {
+        foreach ( [
+                      'timestamp' ,
+                      'nonce',
+                  ] as $item )
+        {
+            if ( !isset( $_GET[ $item ] ) )
+            {
+                throw new ConfigException( '未定义' . $item );
+            }
+        }
+
         $_verifySignatureArr = [
             self::$config['token'] ,
             $_GET['timestamp'] ,
